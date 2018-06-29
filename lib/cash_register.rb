896b9ae3bbc187c8dd7dbe25518041c0ad80f843
louis baptiste <louisvis@ymail.com>
2018-06-29 02:46:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,54 @@
 
+class CashRegister
+attr_accessor :total , :discount
+attr_reader :items
+
+def initialize (discount=0)
+  @discount = discount
+  @total = 0
+  @items = []
+
+end
+
+
+
+
+def add_item (title, price,quantity=1)
+  @title = title
+  @price = price
+  @quantity = quantity
+  @total = @total + price * @quantity
+  @@lastupdate = price * @quantity
+  @items << @title
+   if quantity >1
+    new_quantity = @quantity-1
+    new_quantity.times do @items<< @title
+  end
+
+
+
+
+end
+
+
+
+def void_last_transaction
+  @total = @total- @@lastupdate
+end
+
+
+
+
+
+
+end
+
+
+
+def apply_discount
+  @total = @total-@discount
+  puts "After the discount, the total comes to #{@total}"
+
+end
+
+end
